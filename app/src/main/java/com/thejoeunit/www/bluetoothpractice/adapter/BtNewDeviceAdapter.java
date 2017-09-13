8ea089252a8a68bfe6850a7c3d97086cd00e2b5f
WhanDooS2 <cjh19890212@gmail.com>
2017-09-13 11:18:55 +0900

    새로운 어뎁터 추가구현

---
+++ app/src/main/java/com/thejoeunit/www/bluetoothpractice/adapter/BtNewDeviceAdapter.java
@@ -0,0 +1,45 @@
+package com.thejoeunit.www.bluetoothpractice.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.thejoeunit.www.bluetoothpractice.R;
+import com.thejoeunit.www.bluetoothpractice.data.BtDevice;
+
+import java.util.List;
+
+/**
+ * Created by the on 2017-09-13.
+ */
+
+public class BtNewDeviceAdapter extends ArrayAdapter<BtDevice> {
+
+    Context mContext;
+    List<BtDevice> mList;
+    LayoutInflater inf;
+
+    public BtNewDeviceAdapter(Context context, List<BtDevice> list) {
+        super(context, R.layout.btdevice_list_item, list);
+
+        mContext = context;
+        mList = list;
+        inf = LayoutInflater.from(mContext);
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
+        if (row == null) {
+            row = inf.inflate(R.layout.btdevice_list_item, null);
+        }
+        return row;
+    }
+}
+
